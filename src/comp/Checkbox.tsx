67dea967a3951d88f7a5445ab7a0d61926aa52a5
Kirill A. Khalitov <voronar@gmail.com>
2018-10-23 01:01:10 +0300

    initial

---
+++ src/comp/Checkbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Checkbox } from '@material-ui/core';
+
+class MyCheckbox extends React.PureComponent {
+  public render() {
+    return (
+      <Checkbox>Auto</Checkbox>
+    );
+  }
+}
+
+export default MyCheckbox;
